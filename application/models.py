1e9739e7862aa62e00ac34967569c3f2e02cdbfa
KotiDhana <dhanakotiroxx@gmail.com>
2020-07-04 23:19:49 +0530

    Added DataModel

--- application/models.py
+++ application/models.py
@@ -2,18 +2,18 @@ import flask
 from application import db
 
 class Customer(db.Document):
-    cust_id =   db.IntField( unique=True )
     f_name  =   db.StringField( max_length=20 )
-    l_name  =   db.StringField( max_length=20 )
     phone   =   db.IntField( max_length=12 )
     mail    =   db.StringField( max_length=20 )
     p_name  =   db.StringField( max_length=20 )
     addr_1  =   db.StringField( max_length=25 )
-    addr_2  =   db.StringField( max_length=25 )
     city    =   db.StringField( max_length=20 )
     state   =   db.StringField( max_length=15 )
     pincode =   db.IntField( max_length=6 )
     li_no   =   db.StringField( max_length=10 )
+    cust_id =   db.StringField( max_length=20, unique=True )
+    password=   db.StringField()
+    
 
 class Medicine(db.Document):
     med_id  =   db.StringField( max_length=10, unique=True )
--- application/routes.py
+++ application/routes.py
@@ -1,38 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, abort
 from application import app
-from os import abort
 from application.models import Customer,Medicine,Order
+from application.forms import LoginForm,RegisterForm
+
+
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html',login=True)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    lForm = LoginForm()
+    if lForm.validate_on_submit():
+        userid      =   request.form.get('userid')
+        password    =   lForm.password.data
+
+        user = Customer.objects(user_id=userid).first()
+        if user and password==user.password:
+            flash('Login Successful!','success')
+            return redirect('/index')
+    else:
+        flash('Userid and Password Does not match!...Try again','danger')
+    return render_template('login.html',form=lForm)
+
+
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    rForm = RegisterForm()
+    return render_template('register.html',form=rForm)
 
-    if request.method=='POST':
-        return render_template('register.html')
-    else:
-        return redirect(url_for('home'))
 
-@app.route('/dashboard', methods=['GET','POST'])
+@app.route('/dashboard')
 def manage():
-    if request.method=='POST':
-        flash('Welcome...')
-        return render_template('dashboard.html')
-    else:
-        return redirect('/')
-     
+    return render_template('dashboard.html')
+
+
+
 @app.route('/contact')
 def about():
-    # return render_template('about.html')
-    return abort(404)
+    return render_template('about.html')
+#     return abort(404)
+@app.route('/<code>')
+def not_found(code):
+    return render_template('not_found.html',str=code)
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('not_found.html'),404
     
